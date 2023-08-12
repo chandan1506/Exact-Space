bfe102a7f20f88c504cdcd9c689eaf5b31082a3f
chandan kumar <ck893607@gmail.com>
2023-08-12 11:56:26 +0530

    added post APi for JSON

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,24 @@ app.get("/",(req,res)=>{
     res.json("WELCOME TO BASE API")
 })
 
+//APi for posting JSON
+app.post("/post",(req,res)=>{
+   
+    try {
+         //checking valid JSON or not
+        const payload = req.body
+        if(typeof payload === 'object' && payload !== null){
+            res.status(200).json(payload)
+        }else{
+            res.status(400).json({ error: 'Invalid JSON format' })
+        }
+    } catch (error) {
+        console.error('Error:', error.message);
+        res.status(500).json({ error: 'An error occurred' });
+    }
+   
+})
+
 
 //establishing server
 app.listen(Port,()=>{
